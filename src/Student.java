8642268359a52b3043b2f76b7e3d952bd40f69c1
micha <michaetlt@gmail.com>
2025-08-14 14:17:09 +0300

    Домашнее задание к Лекции 2.6.

---
+++ src/Student.java
@@ -0,0 +1,70 @@
+import java.util.Map;
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private Map<String, Integer> grades;
+
+    public Student(String name, String group, int course, Map<String, Integer> grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(Map<String, Integer> grades) {
+        this.grades = grades;
+    }
+
+    public double getAverageGrade() {
+        if (grades.isEmpty()) return 0.0;
+        int sum = 0;
+        for (int grade : grades.values()) {
+            sum += grade;
+        }
+        return (double) sum / grades.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, course);
+    }
+}
---
+++ src/TelephoneDirectory.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TelephoneDirectory {
+    private Map<String, List<String>> directory;
+
+    public TelephoneDirectory() {
+        this.directory = new HashMap<>();
+    }
+
+    public void add(String name, String number) {
+        directory.putIfAbsent(name, new ArrayList<>());
+        directory.get(name).add(number);
+    }
+
+    public List<String> get (String name) {
+        return directory.getOrDefault(name, Collections.emptyList());
+    }
+}
